1db335f73a3a7cca70d9880d8d72fdd50a3401a1
Ibrahim Salir <112807438+iTheSailor@users.noreply.github.com>
2023-08-20 18:28:29 -0400

    map should be rendering

---
+++ flask_app/static/js/maps.js
@@ -0,0 +1,19 @@
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+({key: "AIzaSyC3wzK_IRzTkYzszfImdCmdYeU02Mbwr4A", v: "weekly"});
+
+function get_user_loc(){
+    return userlat, userlong;
+}
+
+let map, infoWindow;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: userlat, lng: userlong },
+    zoom: 8,
+  });
+}
+
+initMap();
