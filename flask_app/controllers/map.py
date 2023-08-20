bec159a0c3c22127612e82e8ffbf60419fc9a4d9
Ibrahim Salir <112807438+iTheSailor@users.noreply.github.com>
2023-08-20 17:50:28 -0400

    merging current work

--- flask_app/controllers/map.py
+++ flask_app/controllers/map.py
@@ -2,7 +2,9 @@ from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.map_model import Map, Marker
 from flask_app.models.user_model import User
+import googlemaps
 
+<<<<<<< Updated upstream:flask_app/controllers/map.py
 @app.route('/')
 def index():
     return render_template('base.html')
@@ -14,10 +16,40 @@ def initmaps():
     # user = User.get_user_by_id({'id': session['user_id']})
     # maps = Map.get_all_maps_by_user({'user_id': session['user_id']})
     return render_template('maps.html')
+=======
+key = 'AIzaSyC3wzK_IRzTkYzszfImdCmdYeU02Mbwr4A' # we gotta hide this somehow
+
+@app.route('/maps')
+def initmaps():
+    #get user location through geolocation
+    client = googlemaps.Client(key)
+    geocode_result = client.geolocate()
+    print(geocode_result)
+    print(geocode_result['location'])
+    data = {
+        'lat': geocode_result['location']['lat'],
+        'lng': geocode_result['location']['lng']
+    }
+    print(data)
+    return render_template('maps.html', key=key, data=data)
+>>>>>>> Stashed changes:flask_app/controllers/maps.py
 
 @app.route('/maps/<int:map_id>')
 def show_map(map_id):
     pass
+
+@app.route('/newmap')
+def new_map():
+    print('new map')
+    client = googlemaps.Client(key)
+    geocode_result = client.geolocate()
+    print(geocode_result)
+    print(geocode_result['location'])
+    data = {
+        'lat': geocode_result['location']['lat'],
+        'lng': geocode_result['location']['lng']
+    }
+    return render_template('maps.html', key=key, data=data)
 
 @app.route('/maps/create', methods=['POST'])
 def create_map():
--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask_app import app
+<<<<<<< Updated upstream
 from flask_app.controllers import maps
 from flask_app.controllers import map, user
+=======
+from flask_app.controllers import maps, user
+>>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
