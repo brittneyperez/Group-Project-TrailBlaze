26c97d0d327a6bef11a735ca284c8b21fc7740de
Ibrahim Salir <112807438+iTheSailor@users.noreply.github.com>
2023-08-20 17:56:43 -0400

    merging current work

--- flask_app/controllers/map.py
+++ flask_app/controllers/map.py
@@ -4,19 +4,6 @@ from flask_app.models.map_model import Map, Marker
 from flask_app.models.user_model import User
 import googlemaps
 
-<<<<<<< Updated upstream:flask_app/controllers/map.py
-@app.route('/')
-def index():
-    return render_template('base.html')
-
-@app.route('/maps')
-def initmaps():
-    # if 'user_id' not in session:
-    #     return redirect('/')
-    # user = User.get_user_by_id({'id': session['user_id']})
-    # maps = Map.get_all_maps_by_user({'user_id': session['user_id']})
-    return render_template('maps.html')
-=======
 key = 'AIzaSyC3wzK_IRzTkYzszfImdCmdYeU02Mbwr4A' # we gotta hide this somehow
 
 @app.route('/maps')
@@ -32,7 +19,6 @@ def initmaps():
     }
     print(data)
     return render_template('maps.html', key=key, data=data)
->>>>>>> Stashed changes:flask_app/controllers/maps.py
 
 @app.route('/maps/<int:map_id>')
 def show_map(map_id):
--- server.py
+++ server.py
@@ -1,11 +1,5 @@
 from flask_app import app
-<<<<<<< Updated upstream
-from flask_app.controllers import maps
-from flask_app.controllers import map, user
-=======
-from flask_app.controllers import maps, user
->>>>>>> Stashed changes
-
+from flask_app.controllers import map
 
 if __name__ == '__main__':
     app.run(debug=True, host ="localhost", port="8000")
