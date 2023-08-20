62a0e961a0fdd7a0c6750bf50a48fbff0a1e8e2f
iTheSailor <112807438+iTheSailor@users.noreply.github.com>
2023-08-20 18:59:34 -0400

    cleaning after the fix

--- server.py
+++ server.py
@@ -1,10 +1,6 @@
 from flask_app import app
-<<<<<<< HEAD
-from flask_app.controllers import map
-=======
-from flask_app.controllers import maps, users
+from flask_app.controllers import map, user
 
->>>>>>> main
 
 if __name__ == '__main__':
     app.run(debug=True, host ="localhost", port="8000")
