3a1d46a08e4c7113d9349cf77857f2760458016a
iTheSailor <112807438+iTheSailor@users.noreply.github.com>
2023-08-20 14:20:43 -0400

    basic house keeping
    notes in project_log and in discord

---
+++ server.py
@@ -0,0 +1,6 @@
+from flask_app import app
+from flask_app.controllers import maps
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host ="localhost", port="8000")
--- trailblaze/flask_app/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-app.secret_key = 'shhhhhh'
